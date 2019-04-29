d292e157844fbcfbb3246ea4b029e52d7316a26a
BenGunn87 <47829776+BenGunn87@users.noreply.github.com>
2019-04-29 14:33:33 +0500

    Убрал остатки старой реализации

--- blocks/notecard/notecard.js
+++ blocks/notecard/notecard.js
@@ -5,24 +5,16 @@ import _ from './notecard.scss';
 /* eslint-enable */
 
 export class NoteCard extends React.Component {
-  set value ({ title, noteText }) {
-    if (title.length > 25) {
-      title = title.slice(0, 22) + '...';
-    }
-    this.noteTitle.innerHTML = title;
-    if (noteText.length > 40) {
-      noteText = noteText.slice(0, 40) + '...';
-    }
-    this.noteText.innerHTML = noteText;
-  }
   render () {
+    const TITLE_VIEW_MAX_LEN = 25;
+    const NOTETEXT_VIEW_MAX_LEN = 40;
     let title = this.props.title;
     let noteText = this.props.noteText;
-    if (title.length > 25) {
-      title = title.slice(0, 22) + '...';
+    if (title.length > TITLE_VIEW_MAX_LEN) {
+      title = title.slice(0, TITLE_VIEW_MAX_LEN - 3 ) + '...';
     }
-    if (noteText.length > 40) {
-      noteText = noteText.slice(0, 40) + '...';
+    if (noteText.length > NOTETEXT_VIEW_MAX_LEN) {
+      noteText = noteText.slice(0, NOTETEXT_VIEW_MAX_LEN - 3) + '...';
     }
     const activeClass = this.props.selected ? ' note-card-list__card-active' : '';
     return (
