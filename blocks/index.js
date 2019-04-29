b180f136950e69b8bae8dc8ed01bdd09daa4f346
BenGunn87 <47829776+BenGunn87@users.noreply.github.com>
2019-04-29 14:38:17 +0500

    Убрал лишнее

--- blocks/index.js
+++ blocks/index.js
@@ -14,34 +14,8 @@ import _ from './index.scss';
 /* eslint-enable */
 
 window.addEventListener('DOMContentLoaded', () => {
-  const editNote = {
-    title: 'Заголовк Р',
-    noteText: 'Текст заголовка'
-  };
-  const viewCard = {
-    title: 'Заголовок 1',
-    noteText: 'Текст заголовка'
-  };
-  const notes = [{
-    noteid: 1,
-    title: 'Заголовок1',
-    noteText: 'Текст заметки1'
-  }, {
-    noteid: 2,
-    title: 'Заголовок2',
-    noteText: 'Текст заметки2'
-  }, {
-    noteid: 3,
-    title: 'Заголовок3',
-    noteText: 'Текст заметки3'
-  }];
-  const viewForm = {
-    viewCard: viewCard,
-    notes: notes
-  };
-  const activeForm = 'view';
   ReactDOM.render(
-    <MainForm activeForm={activeForm} editNote={editNote} viewForm={viewForm}/>,
+    <MainForm />,
     document.getElementById('root')
   );
 });
